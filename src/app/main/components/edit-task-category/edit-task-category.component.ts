096318842581ffd0dcc1f17486efe102fdaec4b7
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-28 16:57:14 +0300

    create edit task category

---
+++ src/app/main/components/edit-task-category/edit-task-category.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edit-task-category',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './edit-task-category.component.html',
+  styleUrl: './edit-task-category.component.scss'
+})
+export class EditTaskCategoryComponent {
+
+}
