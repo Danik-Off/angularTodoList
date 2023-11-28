e810401b3f41e8518fba780aa4e85691fc2e500e
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-28 18:19:53 +0300

    Complete layout for edit task

--- src/app/main/components/edit-task/edit-task.component.ts
+++ src/app/main/components/edit-task/edit-task.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { TaskСategory } from '../../interfaces/taskCategory';
 @Component({
   selector: 'app-edit-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,DialogModule,InputTextModule, DropdownModule,ButtonModule ],
   templateUrl: './edit-task.component.html',
   styleUrl: './edit-task.component.scss'
 })
 export class EditTaskComponent {
-
+  visible: boolean = true;
+  selectedCategory:any;
+  taskCategories:TaskСategory[] = [];
 }
