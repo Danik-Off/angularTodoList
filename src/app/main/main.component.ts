f0ee160a60b51debc32740ed123c809a56735ee2
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-28 15:54:59 +0300

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TableComponent } from './components/table/table.component';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TableComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
