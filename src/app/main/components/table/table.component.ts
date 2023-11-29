4a95f33f2b5faa6c7f61f865d7ccb293fef4cd23
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-29 09:56:30 +0300

    Update table.component.ts

--- src/app/main/components/table/table.component.ts
+++ src/app/main/components/table/table.component.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { Task } from '../../interfaces/task';
 import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule,TableModule,ButtonModule],
+  imports: [
+    ToolbarModule ,
+    CommonModule,
+    TableModule,
+    ButtonModule,
+
+  ],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent {
   tasks!: Task[];
