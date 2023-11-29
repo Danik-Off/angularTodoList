2a20764afa4b3ac66bc69341ced4e49a9d9289d6
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-29 13:44:39 +0300

    Update auth.service.ts
    добавлена авторизация , регистрация с сохранением в localStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,33 +2,51 @@ import { Injectable } from '@angular/core';
 import { User } from 'src/interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 
 export class AuthService {
   private isAuthenticated: boolean = false;
-  private user:User | undefined ;
+  private user: User | undefined;
 
-  constructor() { }
+  constructor() {}
+
+  getUsers(): User[] {
+    const USERS_JSON = localStorage.getItem('users');
+    return JSON.parse(USERS_JSON ? USERS_JSON : '[]') as User[];
+  }
+
+  //обьект авторизованного пользователя
+  getUser() {
+    return this.user;
+  }
+
+
+  //регистрация нового пользователя
+  register(name: string, login: string, password: string) {
+    this.user = { id: 1, name: name, login: login, password: password };
+    const NEW_USERS = this.getUsers();
+    NEW_USERS.push(this.user);
+    localStorage.setItem('users', JSON.stringify(NEW_USERS));
+  }
 
   //локальная авторизация пользователей
-  login(login:string,password:string) {
-    const USERS_JSON = localStorage.getItem("users");
-    const USERS = JSON.parse(USERS_JSON?USERS_JSON:"[]") as User[];
-    const user = USERS.find(user => user.login === login);
+  login(login: string, password: string) {
+    const user = this.getUsers().find((user) => user.login === login);
 
     if (user && user.password === password) {
       this.isAuthenticated = true;
     } else {
-
     }
   }
 
+  //выйти из аккаунта
   logout() {
     this.isAuthenticated = false;
+    this.user = undefined;
   }
 
+  // проверка , авторизован ли пользователь
   isAuthenticatedUser(): boolean {
     return this.isAuthenticated;
   }
