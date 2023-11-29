a0718cdc65c88230997cf2d219367d6edd831fc7
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-29 15:05:12 +0300

    update login component
    добавлен обработчик нажатия логин

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import {FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../services/auth.service';
 
 
 
@@ -17,9 +18,16 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private authService:AuthService){}
   loginForm = new FormGroup({
-    name: new FormControl(null),
+    login: new FormControl(null),
     password: new FormControl(null),
   });
+  hadlerLoginBtn(){
+    let login = this.loginForm.get("login")?.value
+    let password = this.loginForm.get("password")?.value
+    if(!!login && !!password)
+      this.authService.login(login,password);
+  }
 
 }
