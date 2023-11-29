ad4941d4f4c165eead3cf7b63d335fb277fea26c
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-29 13:45:47 +0300

    update main component
    добавлена панель навигации

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,16 +1,144 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TableComponent } from './components/table/table.component';
 import { EditTaskComponent } from './components/edit-task/edit-task.component';
 import { EditTaskCategoryComponent } from './components/edit-task-category/edit-task-category.component';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,TableComponent,EditTaskComponent,EditTaskCategoryComponent],
+  imports: [CommonModule,TableComponent,EditTaskComponent,EditTaskCategoryComponent,MenubarModule,ButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'File',
+              icon: 'pi pi-fw pi-file',
+              items: [
+                  {
+                      label: 'New',
+                      icon: 'pi pi-fw pi-plus',
+                      items: [
+                          {
+                              label: 'Bookmark',
+                              icon: 'pi pi-fw pi-bookmark'
+                          },
+                          {
+                              label: 'Video',
+                              icon: 'pi pi-fw pi-video'
+                          }
+                      ]
+                  },
+                  {
+                      label: 'Delete',
+                      icon: 'pi pi-fw pi-trash'
+                  },
+                  {
+                      separator: true
+                  },
+                  {
+                      label: 'Export',
+                      icon: 'pi pi-fw pi-external-link'
+                  }
+              ]
+          },
+          {
+              label: 'Edit',
+              icon: 'pi pi-fw pi-pencil',
+              items: [
+                  {
+                      label: 'Left',
+                      icon: 'pi pi-fw pi-align-left'
+                  },
+                  {
+                      label: 'Right',
+                      icon: 'pi pi-fw pi-align-right'
+                  },
+                  {
+                      label: 'Center',
+                      icon: 'pi pi-fw pi-align-center'
+                  },
+                  {
+                      label: 'Justify',
+                      icon: 'pi pi-fw pi-align-justify'
+                  }
+              ]
+          },
+          {
+              label: 'Users',
+              icon: 'pi pi-fw pi-user',
+              items: [
+                  {
+                      label: 'New',
+                      icon: 'pi pi-fw pi-user-plus'
+                  },
+                  {
+                      label: 'Delete',
+                      icon: 'pi pi-fw pi-user-minus'
+                  },
+                  {
+                      label: 'Search',
+                      icon: 'pi pi-fw pi-users',
+                      items: [
+                          {
+                              label: 'Filter',
+                              icon: 'pi pi-fw pi-filter',
+                              items: [
+                                  {
+                                      label: 'Print',
+                                      icon: 'pi pi-fw pi-print'
+                                  }
+                              ]
+                          },
+                          {
+                              icon: 'pi pi-fw pi-bars',
+                              label: 'List'
+                          }
+                      ]
+                  }
+              ]
+          },
+          {
+              label: 'Events',
+              icon: 'pi pi-fw pi-calendar',
+              items: [
+                  {
+                      label: 'Edit',
+                      icon: 'pi pi-fw pi-pencil',
+                      items: [
+                          {
+                              label: 'Save',
+                              icon: 'pi pi-fw pi-calendar-plus'
+                          },
+                          {
+                              label: 'Delete',
+                              icon: 'pi pi-fw pi-calendar-minus'
+                          }
+                      ]
+                  },
+                  {
+                      label: 'Archieve',
+                      icon: 'pi pi-fw pi-calendar-times',
+                      items: [
+                          {
+                              label: 'Remove',
+                              icon: 'pi pi-fw pi-calendar-minus'
+                          }
+                      ]
+                  }
+              ]
+          },
+          {
+              label: 'Quit',
+              icon: 'pi pi-fw pi-power-off'
+          }
+      ];
+  }
 }
