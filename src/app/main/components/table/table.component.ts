b53d162baaef20dc6bf6932e2828d35d1ef3e466
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-30 12:59:13 +0300

    Update table.component.ts
    сортировка
    удаление с окном подтверждения

--- src/app/main/components/table/table.component.ts
+++ src/app/main/components/table/table.component.ts
@@ -5,7 +5,8 @@ import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { Task } from 'src/app/interfaces/task';
-import { AuthService } from 'src/app/services/auth.service';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService, MessageService, ConfirmEventType } from 'primeng/api';
 import { DialogEditTaskService } from 'src/app/services/dialog-edit-task.service';
 import { DialogEditTaskCategoryService } from 'src/app/services/dialog-edit-task-category.service';
 import { TaskService } from 'src/app/services/task.service';
@@ -13,27 +14,62 @@ import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [ToolbarModule, CommonModule, TableModule, ButtonModule],
+  imports: [
+    ToolbarModule,
+    CommonModule,
+    TableModule,
+    ButtonModule,
+    ConfirmDialogModule,
+  ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
+  providers: [ConfirmationService]
 })
 export class TableComponent {
   tasks!: Task[];
   selectedTasks!: Task[];
+
   constructor(
+    private confirmationService: ConfirmationService,
     private dialogService: DialogEditTaskService,
     private dialogCategoriesService: DialogEditTaskCategoryService,
-    private taskService:TaskService,
+    private taskService: TaskService,
   ) {}
-  ngOnInit(){
-    this.taskService.allItems$.subscribe((val)=>{
-      this.tasks  =val;
-    })
+
+  ngOnInit() {
+    this.taskService.allItems$.subscribe((val) => {
+      this.tasks = val;
+      this.selectedTasks = val.filter((task) => {
+        return task.done;
+      });
+      console.log(val);
+    });
+  }
+  handlerOpenEditorTask(id:string){
+    this.dialogService.openDialogEditTask(id);
   }
   handlerOpenEditorTaskForNew() {
     this.dialogService.openDialogEditTask(null);
   }
+
   handlerOpenEditorTaskCatetories() {
     this.dialogCategoriesService.openDialogEditTask();
   }
+
+
+  confirmDelete(id:string) {
+    this.confirmationService.confirm({
+        message: 'Do you want to delete this record?',
+        header: 'Delete Confirmation',
+        icon: 'pi pi-info-circle',
+        accept: () => {
+          this.taskService.deleteItem(id)
+        },
+        reject: () => {
+
+            }
+        }
+    );
+}
+
 }
