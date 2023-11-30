f8d9b6febe52191c3429a24e508192d5ba74aade
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-30 15:40:41 +0300

    Update task-categories.service.ts
    Создана сервис для работы с категориями , add, edit, delete save load

--- src/app/services/task-categories.service.ts
+++ src/app/services/task-categories.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TaskСategory } from '../interfaces/taskCategory';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskCategoriesService {
+  constructor(private authService: AuthService) {}
+  categoties = new BehaviorSubject<TaskСategory[]>([
+    {id:"",title:"test"}
+  ]);
 
-  constructor() { }
+  categories$ = this.categoties.asObservable();
+
+  add(title: string) {
+    const category: TaskСategory = {
+      id: crypto.randomUUID(),
+      title: title,
+    };
+
+    const updatedItems = [category, ...this.categoties.value];
+    this.categoties.next(updatedItems);
+    this.save();
+  }
+
+  edit() {}
+  delete() {}
+
+  save() {
+    let user = this.authService.getUser();
+    localStorage.setItem(
+      'todoItems-' + user?.id,
+      JSON.stringify(this.categoties.value),
+    );
+  }
+
+  load() {
+    let user = this.authService.getUser();
+    const localStorageData = localStorage.getItem('todoItems-' + user?.id);
+    if (localStorageData) {
+      try {
+        const parsedData = JSON.parse(localStorageData) as TaskСategory[];
+
+        [];
+        this.categoties.next(parsedData);
+      } catch (error) {
+        console.error('Ошибка:', error);
+      }
+    }
+  }
 }
