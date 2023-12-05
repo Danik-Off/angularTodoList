75dfc8c6fd01d2430d81eda6459458f330b28958
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-12-05 13:22:01 +0300

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../services/auth.service';
+import { LABEL_BUTTON_LOGIN, LABEL_LINK_REGISTER, LABEL_REGISTER_SUBTEXT, TITLE_LOGIN } from '../shared/constants';
 
 
 
@@ -18,15 +19,23 @@ import { AuthService } from '../services/auth.service';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(private authService:AuthService){}
+
+  titleLabel:string = TITLE_LOGIN;
+  loginLabelBtn:string = LABEL_BUTTON_LOGIN;
+  subtext:string = LABEL_REGISTER_SUBTEXT;
+  linkLabel:string = LABEL_LINK_REGISTER;
+
   loginForm = new FormGroup({
     login: new FormControl(null),
     password: new FormControl(null),
   });
-  hadlerLoginBtn():void {
+
+  constructor(private authService:AuthService){}
+
+  handlerLoginBtn():void {
     let login = this.loginForm.get("login")?.value
     let password = this.loginForm.get("password")?.value
-    if(!!login && !!password)
+    if(login && password)
       this.authService.login(login,password);
   }
 
