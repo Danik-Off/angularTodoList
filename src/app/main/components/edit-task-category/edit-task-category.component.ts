1de75b06d227398fc4a55281c879c4014b56ab9f
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-12-05 13:22:53 +0300

    Update edit-task-category.component.ts

--- src/app/main/components/edit-task-category/edit-task-category.component.ts
+++ src/app/main/components/edit-task-category/edit-task-category.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
-import { TaskСategory } from 'src/app/interfaces/taskCategory';
+import { TaskCategory } from 'src/app/interfaces/taskCategory';
 import { DialogEditTaskCategoryService } from 'src/app/services/dialog-edit-task-category.service';
 import { TaskCategoriesService } from 'src/app/services/task-categories.service';
 import { ItemComponent } from './item/item.component';
+import { Subject, takeUntil } from 'rxjs';
+import { BUTTON_LABEL_ADD, TITLE_EDIT_TASK_CATEGORY} from 'src/app/shared/constants';
 @Component({
   selector: 'app-edit-task-category',
   standalone: true,
@@ -24,25 +26,40 @@ import { ItemComponent } from './item/item.component';
   styleUrl: './edit-task-category.component.scss',
 })
 
-export class EditTaskCategoryComponent {
+export class EditTaskCategoryComponent implements OnInit , OnDestroy {
+
+  addLabelBtn:string = BUTTON_LABEL_ADD;
+  titleDialog:string = TITLE_EDIT_TASK_CATEGORY;
+
+  visible: boolean = true;
+
+  taskCategories: TaskCategory[] = [];
+
+  private ngUnsubscribe = new Subject<void>();
 
   constructor(
     private dialogService: DialogEditTaskCategoryService,
     private taskCategoriesService: TaskCategoriesService,
   ) {}
 
-  visible: boolean = true;
+  ngOnInit(): void {
+    this.dialogService.dialogState$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((value) => {
+        this.visible = value;
+      });
 
-  taskCategories: TaskСategory[] = [];
+    this.taskCategoriesService.categories$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((value) => {
+        this.taskCategories = value;
+      });
+  }
 
-  ngOnInit() {
-    this.dialogService.dialogState$.subscribe((value) => {
-      console.log(value);
-      this.visible = value;
-    });
-    this.taskCategoriesService.categories$.subscribe((value) => {
-      this.taskCategories = value;
-    });
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   handlerAddBtn():void {
