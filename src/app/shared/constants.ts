421c3b34866750825b8412608196a43e927e567b
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-12-05 13:22:21 +0300

    Create constants.ts

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,54 @@
+
+
+//titles
+export const TITLE_PAGE = 'My Angular App';
+export const TITLE_EDIT_TASK_CATEGORY = 'Edit Task Category';
+export const TITLE_EDIT_TASK = 'Edit Task';
+export const TITLE_LOGIN = "Sing in";
+export const TITLE_REGISTER = "Sing up";
+
+//Buttons
+export const BUTTON_LABEL_CANCEL = 'Cancel';
+export const BUTTON_LABEL_SAVE = 'Save';
+export const BUTTON_LABEL_ADD = 'Add';
+export const BUTTON_LABEL_EDIT_TASK_CATEGORIES = 'edit task categories';
+
+
+//TH label table
+export const TH_LABEL_TEXT = 'Add';
+export const TH_LABEL_PRIORITY = 'Priority';
+export const TH_LABEL_CATEGORY = 'Category';
+export const TH_LABEL_DEADLINE = 'Dates';
+
+//Form Edit Task
+export const LABEL_ID = 'Id';
+export const LABEL_TEXT = 'Text';
+export const LABEL_PRIORITY = 'Priority';
+export const LABEL_START_DATE = 'from';
+export const LABEL_END_DATE = 'to';
+export const LABEL_CATEGORY = 'Category';
+
+//Login & register
+export const LABEL_LOGIN = 'Login';
+export const LABEL_PASSWORD = 'Password';
+export const LABEL_BUTTON_LOGIN = 'Login';
+export const LABEL_LOGIN_SUBTEXT = "Already registered?";
+export const LABEL_REGISTER_SUBTEXT = "Not registered yet?";
+export const LABEL_LINK__LOGIN = 'sing in';
+export const LABEL_LINK_REGISTER = 'sing up';
+export const LABEL_REGISTER = 'Register';
+export const LABEL_BUTTON_REGISTER = 'Register';
+export const LABEL_NAME = 'Name';
+export const LABEL_CONFIRM_PASSWORD = 'Repeat password';
+
+
+
+
+
+
+
+
+
+
+
+
