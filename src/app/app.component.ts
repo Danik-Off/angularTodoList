e42fe0928027d934619229bb6231cc97129606a2
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-12-06 12:04:31 +0300

    Update app.component.ts
    текст в константу

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TITLE_PAGE } from './shared/constants';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularTodo';
+  title = TITLE_PAGE;
 }
