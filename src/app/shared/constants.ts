3babf7b7b90873dd902e5bc4dd07ec2b3d11edbf
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-12-10 14:47:26 +0300

    translate to Russian

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,54 +1,40 @@
-
-
 //titles
-export const TITLE_PAGE = 'My Angular App';
-export const TITLE_EDIT_TASK_CATEGORY = 'Edit Task Category';
-export const TITLE_EDIT_TASK = 'Edit Task';
-export const TITLE_LOGIN = "Sing in";
-export const TITLE_REGISTER = "Sing up";
+export const TITLE_PAGE = 'TODO';
+export const TITLE_EDIT_TASK_CATEGORY = 'Редактировать категорию задачи';
+export const TITLE_EDIT_TASK = 'Редактировать задачу';
+export const TITLE_LOGIN = "Войти";
+export const TITLE_REGISTER = "Зарегистрироваться";
 
 //Buttons
-export const BUTTON_LABEL_CANCEL = 'Cancel';
-export const BUTTON_LABEL_SAVE = 'Save';
-export const BUTTON_LABEL_ADD = 'Add';
-export const BUTTON_LABEL_EDIT_TASK_CATEGORIES = 'edit task categories';
+export const BUTTON_LABEL_CANCEL = 'Отмена';
+export const BUTTON_LABEL_SAVE = 'Сохранить';
+export const BUTTON_LABEL_ADD = 'Добавить';
+export const BUTTON_LABEL_EDIT_TASK_CATEGORIES = 'Редактировать категории задач';
 
 
 //TH label table
-export const TH_LABEL_TEXT = 'Add';
-export const TH_LABEL_PRIORITY = 'Priority';
-export const TH_LABEL_CATEGORY = 'Category';
-export const TH_LABEL_DEADLINE = 'Dates';
+export const TH_LABEL_TEXT = 'Текст';
+export const TH_LABEL_PRIORITY = 'Приоритет';
+export const TH_LABEL_CATEGORY = 'Категория';
+export const TH_LABEL_DEADLINE = 'Сроки';
 
 //Form Edit Task
-export const LABEL_ID = 'Id';
-export const LABEL_TEXT = 'Text';
-export const LABEL_PRIORITY = 'Priority';
-export const LABEL_START_DATE = 'from';
-export const LABEL_END_DATE = 'to';
-export const LABEL_CATEGORY = 'Category';
+export const LABEL_ID = 'Идентификатор';
+export const LABEL_TEXT = 'Текст';
+export const LABEL_PRIORITY = 'Приоритет';
+export const LABEL_START_DATE = 'Начало';
+export const LABEL_END_DATE = 'Конец';
+export const LABEL_CATEGORY = 'Категория';
 
 //Login & register
-export const LABEL_LOGIN = 'Login';
-export const LABEL_PASSWORD = 'Password';
-export const LABEL_BUTTON_LOGIN = 'Login';
-export const LABEL_LOGIN_SUBTEXT = "Already registered?";
-export const LABEL_REGISTER_SUBTEXT = "Not registered yet?";
-export const LABEL_LINK__LOGIN = 'sing in';
-export const LABEL_LINK_REGISTER = 'sing up';
-export const LABEL_REGISTER = 'Register';
-export const LABEL_BUTTON_REGISTER = 'Register';
-export const LABEL_NAME = 'Name';
-export const LABEL_CONFIRM_PASSWORD = 'Repeat password';
-
-
-
-
-
-
-
-
-
-
-
-
+export const LABEL_LOGIN = 'Логин';
+export const LABEL_PASSWORD = 'Пароль';
+export const LABEL_BUTTON_LOGIN = 'Войти';
+export const LABEL_LOGIN_SUBTEXT = "Уже зарегистрированы?";
+export const LABEL_REGISTER_SUBTEXT = "Еще не зарегистрированы?";
+export const LABEL_LINK__LOGIN = 'войти';
+export const LABEL_LINK_REGISTER = 'зарегистрироваться';
+export const LABEL_REGISTER = 'Регистрация';
+export const LABEL_BUTTON_REGISTER = 'Зарегистрироваться';
+export const LABEL_NAME = 'Имя';
+export const LABEL_CONFIRM_PASSWORD = 'Повторите пароль';
