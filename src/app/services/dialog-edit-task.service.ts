dc8b4733f40fd8c54648237b30505cdc4b25e7e7
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-12-11 12:24:01 +0300

    Update dialog-edit-task.service.ts

--- src/app/services/dialog-edit-task.service.ts
+++ src/app/services/dialog-edit-task.service.ts
@@ -5,15 +5,19 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class DialogEditTaskService {
-  dialogState$!: Observable<string | null>;
 
-  private dialogStateSubject = new BehaviorSubject<string | null>(null);
+  dialogState$!: Observable<boolean|string>;
+
+  private dialogStateSubject = new BehaviorSubject<boolean|string>(false);
 
   constructor() {
     this.dialogState$ = this.dialogStateSubject.asObservable();
   }
 
-  openDialogEditTask(id: string | null): void {
+  openDialogEditTask(id: string | boolean): void {
     this.dialogStateSubject.next(id);
+  }
+  closeDialogEditTask(): void {
+    this.dialogStateSubject.next(false);
   }
 }
